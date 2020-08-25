f6ee0e702702b7435fc25d60af680b0045bcd5bc
Victor Torres <torres.victor136@gmail.com>
2020-08-25 18:56:50 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,25 @@
+def using_push(array, string)
+  array.push(string)
+end 
+
+def using_pop(array, element)
+  array.unshift(element)
+end 
+
+def using_pop(array)
+  array.pop 
+end 
+
+def pop_with_args(array)
+  array.pop2
+end 
+
+def using_shift(array)
+  array.shift 
+end 
+
+def shift_with_args(array)
+  array.shift2 
+end 
+
+  
